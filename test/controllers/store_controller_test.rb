87acd0615bd5267e7a979a5d9920db0e0dc7a6b7
Якунин Иван <iv.ya@list.ru>
2025-05-14 11:19:00 +0300

    Functional tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select "nav a", minimum: 4
+    assert_select "ul li", 3
+    assert_select "h2", "ruby"
+    assert_select ".price", /\$[,\d]+\.\d\d/
   end
 end
