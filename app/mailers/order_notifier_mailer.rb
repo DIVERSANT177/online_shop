ae3045bfbf680f5c6cc979bad29504da3fe2fddb
Якунин Иван <iv.ya@list.ru>
2025-05-16 14:43:58 +0300

    Partials for line_items in order and cart

---
+++ app/mailers/order_notifier_mailer.rb
@@ -0,0 +1,25 @@
+class OrderNotifierMailer < ApplicationMailer
+  default from: "notifications@example.com"
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.received.subject
+  #
+  def received(order)
+    @greeting = "Привет"
+    @order = order
+
+    mail to: order.email, subject: "Подтверждение заказа"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
